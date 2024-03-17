19f277e3ef921365b383586d1df9483b40a3901d
viridis <viridis959@gmail.com>
2024-03-18 01:26:53 +0800

    Add mac preferences & vscode keybindings

---
+++ mac/system_preferences.sh
@@ -0,0 +1,11 @@
+defaults write "Apple Global Domain" NSUserKeyEquivalents ' {
+    "Undo"  = "^z";
+    "Redo"  = "^$z";
+    "Cut"   = "^x";
+    "Copy"  = "^c";
+    "Paste" = "^v";
+    "Find"  = "^f";
+    "Find..."  = "^f";
+    "Replace"  = "^r";
+    "Save" = "^s";
+}'
