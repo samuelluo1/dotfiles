a0a5071fd57cbdf556c152f4cc8134bb94c113c0
viridis <viridis959@gmail.com>
2024-03-18 01:33:47 +0800

    change mac preferences

--- mac/system_preferences.sh
+++ mac/system_preferences.sh
@@ -4,6 +4,8 @@ defaults write "Apple Global Domain" NSUserKeyEquivalents ' {
     "Cut"   = "^x";
     "Copy"  = "^c";
     "Paste" = "^v";
+    "Paste and Match Style" = "^$v";
+    "Select All" = "^a";
     "Find"  = "^f";
     "Find..."  = "^f";
     "Replace"  = "^r";
